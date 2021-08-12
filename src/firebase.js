493df4f9178abe2f5c358a0eec8dca415cbc0566
Joegie Wagwag <red.mrjhon8@gmail.com>
2021-08-12 16:26:14 +0800

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import '@firebase/database';
+
+const config = {
+  // Paste here your firebase configuration
+  // 1. Go to your https://console.firebase.google.com/
+  // 2. Go to your Project Settings
+  // 3. On General tab, scroll down to your Web Apps
+  // 4. On SDK setup and configuration, copy what's under the radio Config button 
+};
+
+const firebaseApp = firebase.initializeApp(config);
+const goalCoach = firebase.database().ref(/*TableName*/);
+const goalComplete = firebase.database().ref(/*TableName*/);
+const listUsers = firebase.database().ref(/*TableName*/);
+
+export { firebase, goalCoach, goalComplete, listUsers }

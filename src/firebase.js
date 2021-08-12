9fd732f860bcde62186a8a8f9647c88603b754ed
jhoe08 <red.mrjhon8@gmail.com>
2021-08-12 15:33:03 +0800

    Removed data

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,18 @@ import 'firebase/auth';
 import '@firebase/database';
 
 const config = {
-  apiKey: "AIzaSyBhKTQRa9aq7ZfidVnSft3cD-yWMfN_1_Y",
-  authDomain: "goalcoach-2c3a0.firebaseapp.com",
-  projectId: "goalcoach-2c3a0",
-  storageBucket: "goalcoach-2c3a0.appspot.com",
-  messagingSenderId: "640998819214",
-  appId: "1:640998819214:web:72e1c875f61e49f7bad0df",
-  measurementId: "G-ZBLH1J34WK"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(config);
-const goalCoach = firebase.database().ref('goalCoach');
-const goalComplete = firebase.database().ref('completeGoal');
-const listUsers = firebase.database().ref('listUsers');
+const goalCoach = firebase.database().ref();
+const goalComplete = firebase.database().ref();
+const listUsers = firebase.database().ref();
 
 export { firebase, goalCoach, goalComplete, listUsers }

b2ae157766d0441a2f7432f85ef5bc7236fcff36
Joegie Wagwag <red.mrjhon8@gmail.com>
2021-08-12 16:14:17 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,16 @@ import 'firebase/auth';
 import '@firebase/database';
 
 const config = {
-  // Copy paste your
+  // Paste here your firebase configuration
+  // 1. Go to your https://console.firebase.google.com/
+  // 2. Go to your Project Settings
+  // 3. On General tab, scroll down to your Web Apps
+  // 4. On SDK setup and configuration, copy what's under the radio Config button
 };
 
 const firebaseApp = firebase.initializeApp(config);
-const goalCoach = firebase.database().ref('goalCoach');
-const goalComplete = firebase.database().ref('completeGoal');
-const listUsers = firebase.database().ref('listUsers');
+const goalCoach = firebase.database().ref(/*Table Name*/);
+const goalComplete = firebase.database().ref(/*Table Name*/);
+const listUsers = firebase.database().ref(/*Table Name*/);
 
 export { firebase, goalCoach, goalComplete, listUsers }
